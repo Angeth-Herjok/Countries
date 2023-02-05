8fe6732390a5b5f443fcc8e7d19498ee2bf6b5cf
Angeth-Herjok <angethherjok@gmail.com>
2023-02-05 19:39:06 +0300

    Uganda

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    var name ="Uganda"
+    println(name)
+    var independence ="1962"
+    var population ="2345679"
+    var land ="56798km"
+    println(independence)
+    println(population)
+    println(land)
+
+}
